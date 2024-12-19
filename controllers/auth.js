1233a012fc558b638bb74147e1ce57737a7e93fa
johnoro <officialjohnorourke@gmail.com>
2024-12-18 21:03:15 -0500

    sets up bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,15 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const bcrypt = require('bcryptjs');
+
 module.exports = {
 	register: async (req, res) => {
-		res.send('register');
+		const { name, email, password } = req.body;
+		const salt = await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS) || 10);
+		const passwordHash = await bcrypt.hash(password, salt);
+
+		const user = await User.create({ name, email, password: passwordHash });
+		res.status(StatusCodes.CREATED).json({ user });
 	},
 	login: async (req, res) => {
 		res.send('login');
