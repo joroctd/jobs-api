5c7d820bc5eb869fe0d124f3106b386282d6815d
johnoro <officialjohnorourke@gmail.com>
2024-12-24 14:21:51 -0500

    builds out functionality along with delete

--- public/jobs.js
+++ public/jobs.js
@@ -25,12 +25,92 @@ export const handleJobs = () => {
 			if (e.target === addJob) {
 				showAddEdit(null);
 			} else if (e.target === logoff) {
+				setToken(null);
+
+				message.textContent = 'You have been logged off.';
+
+				jobsTable.replaceChildren([jobsTableHeader]);
+
 				showLoginRegister();
+			} else if (e.target.classList.contains('editButton')) {
+				message.textContent = '';
+				showAddEdit(e.target.dataset.id);
+			} else if (e.target.classList.contains('deleteButton')) {
+				message.textContent = '';
+				deleteJob(e.target.dataset.id);
 			}
 		}
 	});
 };
 
+const deleteJob = async jobId => {
+	try {
+		enableInput(false);
+
+		const response = await fetch(`/api/v1/jobs/${jobId}`, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`
+			}
+		});
+
+		if (response.status === 204) {
+			message.textContent = `Deleton of ${jobId} was successful.`;
+			await showJobs();
+		} else {
+			const data = await response.json();
+			message.textContent = data.msg;
+		}
+	} catch (err) {
+		console.log(err);
+		message.textContent = 'A communication error occurred.';
+	}
+	enableInput(true);
+};
+
 export const showJobs = async () => {
+	try {
+		enableInput(false);
+
+		const response = await fetch('/api/v1/jobs', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`
+			}
+		});
+
+		const data = await response.json();
+		let children = [jobsTableHeader];
+
+		if (response.status === 200) {
+			if (data.count === 0) {
+				jobsTable.replaceChildren(...children); // clear this for safety
+			} else {
+				for (let i = 0; i < data.jobs.length; i++) {
+					let rowEntry = document.createElement('tr');
+
+					let editButton = `<td><button type="button" class="editButton" data-id=${data.jobs[i]._id}>edit</button></td>`;
+					let deleteButton = `<td><button type="button" class="deleteButton" data-id=${data.jobs[i]._id}>delete</button></td>`;
+					let rowHTML = `
+			  <td>${data.jobs[i].company}</td>
+			  <td>${data.jobs[i].position}</td>
+			  <td>${data.jobs[i].status}</td>
+			  <div>${editButton}${deleteButton}</div>`;
+
+					rowEntry.innerHTML = rowHTML;
+					children.push(rowEntry);
+				}
+				jobsTable.replaceChildren(...children);
+			}
+		} else {
+			message.textContent = data.msg;
+		}
+	} catch (err) {
+		console.log(err);
+		message.textContent = 'A communication error occurred.';
+	}
+	enableInput(true);
 	setDiv(jobsDiv);
 };
