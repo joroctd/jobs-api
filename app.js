618dff31ebd04d468b5287f1cc764be9886ad0ed
johnoro <officialjohnorourke@gmail.com>
2025-01-20 20:21:04 -0500

    sets up security middleware and api docs

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
-const app = express();
+
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const cors = require('cors');
+
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerYaml = YAML.load('./swagger.yaml');
+
 const connectDB = require('./db/connect');
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
@@ -11,13 +20,36 @@ const authMiddleware = require('./middleware/authentication');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(express.json());
-// extra packages
+const app = express();
+
+// How "app.get('env')" works:
+// app.get('env') = process.env.NODE_ENV ? process.env.NODE_ENV || 'development'
+const isProduction = app.get('env') === 'production';
+if (isProduction) {
+	app.set('trust proxy', 1);
+}
+
+// security middleware
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100 // max requests, per IP, per amount of time above
+	}),
+	express.json(),
+	helmet(),
+	hpp()
+);
+
+if (isProduction) {
+	app.use(cors());
+}
 
 // routes
 app.get('/', (req, res) => {
 	res.send('jobs api');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerYaml));
 
 const apiRouter = express.Router();
 app.use('/api/v1', apiRouter);
